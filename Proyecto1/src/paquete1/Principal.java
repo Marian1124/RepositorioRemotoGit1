0d3aa80f44bfe631519c77532444908fe16eccf5
Usuario <Usuario@LAPTOP-GR3535BK>
2025-05-01 13:58:50 +0200

    Segundo commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println ("Estamos haciendo control de versines con Git");
+		System.out.println ("Hacemos un cambio en la versión");
 
 	}
 
